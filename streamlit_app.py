1136575220236a85424501ad6220fb3936c9ab63
padrinodonvitto <padrinodonvitto@yahoo.es>
2026-02-05 11:18:00 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,4 +44,4 @@ if ingredients_list:
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 #st.text(smoothiefroot_response.json())
-st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_with=True)
+st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
